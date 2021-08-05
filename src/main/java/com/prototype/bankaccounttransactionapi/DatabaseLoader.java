254ffe0b7755462fec565ea7639f3ea4d5d9dbbc
Yavuz Selim Polat <yspolat@gmail.com>
2021-08-05 21:59:10 +0300

    Added example customers for demo

--- src/main/java/com/prototype/bankaccounttransactionapi/DatabaseLoader.java
+++ src/main/java/com/prototype/bankaccounttransactionapi/DatabaseLoader.java
@@ -1,6 +1,7 @@
 package com.prototype.bankaccounttransactionapi;
 
 import com.prototype.bankaccounttransactionapi.model.Customer;
+import com.prototype.bankaccounttransactionapi.repository.AccountRepository;
 import com.prototype.bankaccounttransactionapi.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 class DatabaseLoader {
     @Bean
-    CommandLineRunner init(CustomerRepository repository) {
+    CommandLineRunner init(CustomerRepository customerRepository) {
 
         return args -> {
-            repository.save(new Customer("John","Doe",1000,0,null));
+            customerRepository.save(new Customer("John","Doe",500, null));
+            customerRepository.save(new Customer("Jane","Doe",1000, null));
         };
     }
 }
